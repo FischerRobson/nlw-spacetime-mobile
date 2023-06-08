67fbffdd33cbe0d5a0d5b64a8ebdadb436b5f909
Fischer <fischerrobson@gmail.com>
2023-06-07 23:59:01 -0300

    static memories page

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,9 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView, Text, TouchableOpacity, Image } from 'react-native'
+import NlwSpacetimeLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link, useRouter } from 'expo-router'
+import Icon from '@expo/vector-icons/Feather'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
 
 export default function Memories() {
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
+  async function singOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NlwSpacetimeLogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={singOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50" />
+            <Text className="font-body text-xs text-gray-50">teste</Text>
+          </View>
+
+          <View className="space-y-4 px-8">
+            <Image
+              className="aspect-video w-full rounded-lg"
+              source={{
+                uri: 'http://192.168.0.108:3333/uploads/cd6d3e20-ae04-4e98-a603-0e9a4548159c.gif',
+              }}
+              alt=""
+            />
+          </View>
+          <Text className="font-body text-base leading-relaxed text-gray-100">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            blanditiis voluptas accusamus vitae reiciendis veritatis eaque
+            praesentium, amet doloribus repellendus. Officiis aperiam nemo
+            quisquam eveniet consequatur mollitia eius voluptatem deleniti?
+          </Text>
+          <Link href="/memories/id" asChild>
+            <TouchableOpacity className="flex-row items-center gap-2">
+              <Text className="font-body text-sm text-gray-200">Ler mais</Text>
+              <Icon name="arrow-right" size={16} color="#9e9ea0" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
